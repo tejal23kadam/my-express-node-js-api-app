15316ed070123aed4685d5989f54a7845877bff4
tejal23kadam <tejalmchandlekar@gmail.com>
2024-12-02 12:14:50 +0530

    added code for designing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './App.css';
+
 
 
 function App() {
   const [data, setData] = useState(null);
-  //axios.defaults.proxy.host = "http://localhost:4000"
+  const [apidata, apisetData] = useState(null);
   useEffect(() => {
-    axios.get('/api/data/', {
+    axios.get('/about', {
       proxy: {
         host: 'localhost',
         port: 4000
@@ -14,26 +16,83 @@ function App() {
     })
       .then(response => {
         setData(response.data);
+        console.log(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+  useEffect(() => {
+    axios.get('/api/data', {
+      proxy: {
+        host: 'localhost',
+        port: 4000
+      }
+    })
+      .then(response => {
+        apisetData(response.data);
+        console.log(data);
       })
       .catch(error => {
         console.error(error);
       });
   }, []);
 
+
+
+
+
+
+  
   return (
-    <div className="App">
-      <h1>React and Node.js Integration</h1>
-      {(data) ? (
-        data.map((data) => (
-          <div key={data}>
-            {data}
-          </div>
-        ))
-      ) : (
-        <h3>empty Array</h3>
-      )
-      }
-    </div>
+    <>
+
+
+    
+      <div className='container'>
+        <div className='pro-container'>
+          {(apidata) ?
+            (
+              (apidata.map((data) => (
+                <div className="pro" key={data.id} >
+                  <div class="des" >
+                    <img src={data.image} alt="noImage" />
+                    <h5 className="overme">{data.title} </h5>
+                    <div>
+                      {
+                        (data.discount) ? (
+                          <div style={{ display: "flex" }}>
+                            <h5><s>{data.price}</s> </h5>
+                            <h4>${Math.trunc(data.price - ((data.price * data.discount) / 100))}</h4>
+                            <div style={{ display: "flex", paddingTop: "6px" }}>
+                              <p class="discount">{data.discount}%</p>
+                              <p>off no</p>
+                            </div>
+                          </div>
+
+                        ) :
+                          (
+                            <h4>${data.price}</h4>
+                          )
+                      }
+                    </div>
+                  </div>
+               
+                </div>
+              )))
+            ) :
+            (<h1>data is missing</h1>)
+          }
+        </div>
+
+      </div >
+
+    </>
+
+
+
+
+
   );
 }
 
